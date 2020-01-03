4036ec8889a262feb21286a4d88d1defbcb512bb
ChenJingZhuo <3279838729@qq.com>
2020-01-03 21:26:36 +0800

    第一次提交

---
+++ app/src/main/java/com/example/reflect/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.reflect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        String TAG = "MainActivity";
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            // 创建对象
+            ReflectClass.reflectNewInstance();
+
+            // 反射私有的构造方法
+            ReflectClass.reflectPrivateConstructor();
+
+            // 反射私有属性
+            ReflectClass.reflectPrivateField();
+
+            // 反射私有方法
+            ReflectClass.reflectPrivateMethod();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        Log.d(TAG," zenmode = " + ReflectClass.getZenMode());
+    }
+}
